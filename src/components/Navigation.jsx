df19a3f9f71774997b0166bb985e6eca966c95e2
Ash <ashleighb.jjd@gmail.com>
2025-01-30 01:40:45 -0500

    Updated Navigation tabs

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,43 @@ import { Link, useLocation } from 'react-router-dom';
 
 function Navigation() {
     const currentPage = useLocation().pathname;
+
+    return (
+        <ul className="nav navigation">
+            <li className="nav-item">
+                <Link
+                    to="/"
+                    class={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >
+                    About me
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Portfolio"
+                    class={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Contact"
+                    class={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Resume"
+                    class={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </Link>
+            </li>
+        </ul>
+    );
 }
 
 export default Navigation;
